d1db0952c43fb26837d1c53b4807f21fd6e10821
Yahir-07 <20251168@uthh.edu.mx>
2026-06-18 11:32:04 -0600

    hola

--- prySistemaEscolar/FrmPrincipal.Designer.cs
+++ prySistemaEscolar/FrmPrincipal.Designer.cs
@@ -20,157 +20,174 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
-            pcbMenu = new PictureBox();
-            pcbTitulo = new PictureBox();
-            pcbAlumnos = new PictureBox();
-            pcbCarreras = new PictureBox();
-            pcbUsuarios = new PictureBox();
-            pcbDocentes = new PictureBox();
-            pnlContenedor = new Panel();
-            pcbExit = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pcbMenu).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pcbTitulo).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pcbAlumnos).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pcbCarreras).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pcbUsuarios).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pcbDocentes).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pcbExit).BeginInit();
-            SuspendLayout();
-            // 
-            // pcbMenu
-            // 
-            pcbMenu.Image = (Image)resources.GetObject("pcbMenu.Image");
-            pcbMenu.Location = new Point(12, 12);
-            pcbMenu.Name = "pcbMenu";
-            pcbMenu.Size = new Size(417, 967);
-            pcbMenu.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbMenu.TabIndex = 0;
-            pcbMenu.TabStop = false;
-            // 
-            // pcbTitulo
-            // 
-            pcbTitulo.Image = (Image)resources.GetObject("pcbTitulo.Image");
-            pcbTitulo.Location = new Point(462, 12);
-            pcbTitulo.Name = "pcbTitulo";
-            pcbTitulo.Size = new Size(1446, 216);
-            pcbTitulo.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbTitulo.TabIndex = 1;
-            pcbTitulo.TabStop = false;
-            // 
-            // pcbAlumnos
-            // 
-            pcbAlumnos.Image = (Image)resources.GetObject("pcbAlumnos.Image");
-            pcbAlumnos.Location = new Point(80, 187);
-            pcbAlumnos.Name = "pcbAlumnos";
-            pcbAlumnos.Size = new Size(260, 109);
-            pcbAlumnos.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbAlumnos.TabIndex = 2;
-            pcbAlumnos.TabStop = false;
-            pcbAlumnos.MouseDown += pcbGeneral_MouseDown;
-            pcbAlumnos.MouseUp += pcbGeneral_MouseUp;
-            // 
-            // pcbCarreras
-            // 
-            pcbCarreras.Image = (Image)resources.GetObject("pcbCarreras.Image");
-            pcbCarreras.Location = new Point(80, 334);
-            pcbCarreras.Name = "pcbCarreras";
-            pcbCarreras.Size = new Size(260, 109);
-            pcbCarreras.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbCarreras.TabIndex = 3;
-            pcbCarreras.TabStop = false;
-            pcbCarreras.MouseDown += pcbGeneral_MouseDown;
-            pcbCarreras.MouseUp += pcbGeneral_MouseUp;
-            // 
-            // pcbUsuarios
-            // 
-            pcbUsuarios.Image = (Image)resources.GetObject("pcbUsuarios.Image");
-            pcbUsuarios.Location = new Point(80, 650);
-            pcbUsuarios.Name = "pcbUsuarios";
-            pcbUsuarios.Size = new Size(260, 109);
-            pcbUsuarios.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbUsuarios.TabIndex = 4;
-            pcbUsuarios.TabStop = false;
-            pcbUsuarios.MouseDown += pcbGeneral_MouseDown;
-            pcbUsuarios.MouseUp += pcbGeneral_MouseUp;
-            // 
-            // pcbDocentes
-            // 
-            pcbDocentes.Image = (Image)resources.GetObject("pcbDocentes.Image");
-            pcbDocentes.Location = new Point(80, 495);
-            pcbDocentes.Name = "pcbDocentes";
-            pcbDocentes.Size = new Size(260, 109);
-            pcbDocentes.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbDocentes.TabIndex = 5;
-            pcbDocentes.TabStop = false;
-            pcbDocentes.MouseDown += pcbGeneral_MouseDown;
-            pcbDocentes.MouseUp += pcbGeneral_MouseUp;
-            // 
-            // pnlContenedor
-            // 
-            pnlContenedor.BackColor = Color.White;
-            pnlContenedor.ForeColor = SystemColors.ControlText;
-            pnlContenedor.Location = new Point(462, 261);
-            pnlContenedor.Name = "pnlContenedor";
-            pnlContenedor.Size = new Size(1446, 718);
-            pnlContenedor.TabIndex = 6;
-            // 
-            // pcbExit
-            // 
-            pcbExit.Image = (Image)resources.GetObject("pcbExit.Image");
-            pcbExit.Location = new Point(80, 802);
-            pcbExit.Name = "pcbExit";
-            pcbExit.Size = new Size(260, 109);
-            pcbExit.SizeMode = PictureBoxSizeMode.StretchImage;
-            pcbExit.TabIndex = 0;
-            pcbExit.TabStop = false;
-            pcbExit.MouseDown += pcbGeneral_MouseDown;
-            pcbExit.MouseUp += pcbGeneral_MouseUp;
-            // 
-            // FrmPrincipal
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(51, 63, 80);
-            ClientSize = new Size(1920, 991);
-            Controls.Add(pcbExit);
-            Controls.Add(pnlContenedor);
-            Controls.Add(pcbDocentes);
-            Controls.Add(pcbUsuarios);
-            Controls.Add(pcbCarreras);
-            Controls.Add(pcbAlumnos);
-            Controls.Add(pcbTitulo);
-            Controls.Add(pcbMenu);
-            Name = "FrmPrincipal";
-            Text = "Sistema Escolar";
-            WindowState = FormWindowState.Maximized;
-            ((System.ComponentModel.ISupportInitialize)pcbMenu).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pcbTitulo).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pcbAlumnos).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pcbCarreras).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pcbUsuarios).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pcbDocentes).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pcbExit).EndInit();
-            ResumeLayout(false);
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmPrincipal));
+			pcbMenu = new PictureBox();
+			pcbTitulo = new PictureBox();
+			pcbAlumnos = new PictureBox();
+			pcbCarreras = new PictureBox();
+			pcbTutores = new PictureBox();
+			pcbDocentes = new PictureBox();
+			pnlContenedor = new Panel();
+			pcbUsuarios = new PictureBox();
+			((System.ComponentModel.ISupportInitialize)pcbMenu).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pcbTitulo).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pcbAlumnos).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pcbCarreras).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pcbTutores).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pcbDocentes).BeginInit();
+			((System.ComponentModel.ISupportInitialize)pcbUsuarios).BeginInit();
+			SuspendLayout();
+			// 
+			// pcbMenu
+			// 
+			pcbMenu.Image = (Image)resources.GetObject("pcbMenu.Image");
+			pcbMenu.Location = new Point(10, 9);
+			pcbMenu.Margin = new Padding(3, 2, 3, 2);
+			pcbMenu.Name = "pcbMenu";
+			pcbMenu.Size = new Size(365, 725);
+			pcbMenu.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbMenu.TabIndex = 0;
+			pcbMenu.TabStop = false;
+			// 
+			// pcbTitulo
+			// 
+			pcbTitulo.Image = (Image)resources.GetObject("pcbTitulo.Image");
+			pcbTitulo.Location = new Point(404, 9);
+			pcbTitulo.Margin = new Padding(3, 2, 3, 2);
+			pcbTitulo.Name = "pcbTitulo";
+			pcbTitulo.Size = new Size(1265, 162);
+			pcbTitulo.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbTitulo.TabIndex = 1;
+			pcbTitulo.TabStop = false;
+			// 
+			// pcbAlumnos
+			// 
+			pcbAlumnos.Image = (Image)resources.GetObject("pcbAlumnos.Image");
+			pcbAlumnos.Location = new Point(70, 140);
+			pcbAlumnos.Margin = new Padding(3, 2, 3, 2);
+			pcbAlumnos.Name = "pcbAlumnos";
+			pcbAlumnos.Size = new Size(228, 82);
+			pcbAlumnos.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbAlumnos.TabIndex = 2;
+			pcbAlumnos.TabStop = false;
+			pcbAlumnos.Click += frmPrincipal_Load;
+			pcbAlumnos.MouseDown += pcbGeneral_MouseDown;
+			pcbAlumnos.MouseUp += pcbGeneral_MouseUp;
+			// 
+			// pcbCarreras
+			// 
+			pcbCarreras.Image = (Image)resources.GetObject("pcbCarreras.Image");
+			pcbCarreras.Location = new Point(70, 250);
+			pcbCarreras.Margin = new Padding(3, 2, 3, 2);
+			pcbCarreras.Name = "pcbCarreras";
+			pcbCarreras.Size = new Size(228, 82);
+			pcbCarreras.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbCarreras.TabIndex = 3;
+			pcbCarreras.TabStop = false;
+			pcbCarreras.Click += pcbCarreras_Click;
+			pcbCarreras.MouseDown += pcbGeneral_MouseDown;
+			pcbCarreras.MouseUp += pcbGeneral_MouseUp;
+			// 
+			// pcbTutores
+			// 
+			pcbTutores.Image = (Image)resources.GetObject("pcbTutores.Image");
+			pcbTutores.Location = new Point(70, 481);
+			pcbTutores.Margin = new Padding(3, 2, 3, 2);
+			pcbTutores.Name = "pcbTutores";
+			pcbTutores.Size = new Size(228, 82);
+			pcbTutores.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbTutores.TabIndex = 4;
+			pcbTutores.TabStop = false;
+			pcbTutores.Click += frmPrincipal_Load;
+			pcbTutores.MouseDown += pcbGeneral_MouseDown;
+			pcbTutores.MouseUp += pcbGeneral_MouseUp;
+			// 
+			// pcbDocentes
+			// 
+			pcbDocentes.Image = (Image)resources.GetObject("pcbDocentes.Image");
+			pcbDocentes.Location = new Point(70, 371);
+			pcbDocentes.Margin = new Padding(3, 2, 3, 2);
+			pcbDocentes.Name = "pcbDocentes";
+			pcbDocentes.Size = new Size(228, 82);
+			pcbDocentes.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbDocentes.TabIndex = 5;
+			pcbDocentes.TabStop = false;
+			pcbDocentes.Click += frmPrincipal_Load;
+			pcbDocentes.MouseDown += pcbGeneral_MouseDown;
+			pcbDocentes.MouseUp += pcbGeneral_MouseUp;
+			// 
+			// pnlContenedor
+			// 
+			pnlContenedor.BackColor = Color.White;
+			pnlContenedor.ForeColor = SystemColors.ControlText;
+			pnlContenedor.Location = new Point(404, 196);
+			pnlContenedor.Margin = new Padding(3, 2, 3, 2);
+			pnlContenedor.Name = "pnlContenedor";
+			pnlContenedor.Size = new Size(1265, 538);
+			pnlContenedor.TabIndex = 6;
+			// 
+			// pcbUsuarios
+			// 
+			pcbUsuarios.Image = (Image)resources.GetObject("pcbUsuarios.Image");
+			pcbUsuarios.Location = new Point(70, 584);
+			pcbUsuarios.Margin = new Padding(3, 2, 3, 2);
+			pcbUsuarios.Name = "pcbUsuarios";
+			pcbUsuarios.Size = new Size(228, 82);
+			pcbUsuarios.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbUsuarios.TabIndex = 0;
+			pcbUsuarios.TabStop = false;
+			pcbUsuarios.Click += frmPrincipal_Load;
+			pcbUsuarios.MouseDown += pcbGeneral_MouseDown;
+			pcbUsuarios.MouseUp += pcbGeneral_MouseUp;
+			// 
+			// FrmPrincipal
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			BackColor = Color.FromArgb(51, 63, 80);
+			CausesValidation = false;
+			ClientSize = new Size(1370, 700);
+			Controls.Add(pcbUsuarios);
+			Controls.Add(pnlContenedor);
+			Controls.Add(pcbDocentes);
+			Controls.Add(pcbTutores);
+			Controls.Add(pcbCarreras);
+			Controls.Add(pcbAlumnos);
+			Controls.Add(pcbTitulo);
+			Controls.Add(pcbMenu);
+			Margin = new Padding(3, 2, 3, 2);
+			Name = "FrmPrincipal";
+			Text = "Sistema Escolar";
+			WindowState = FormWindowState.Maximized;
+			Load += frmPrincipal_Load;
+			((System.ComponentModel.ISupportInitialize)pcbMenu).EndInit();
+			((System.ComponentModel.ISupportInitialize)pcbTitulo).EndInit();
+			((System.ComponentModel.ISupportInitialize)pcbAlumnos).EndInit();
+			((System.ComponentModel.ISupportInitialize)pcbCarreras).EndInit();
+			((System.ComponentModel.ISupportInitialize)pcbTutores).EndInit();
+			((System.ComponentModel.ISupportInitialize)pcbDocentes).EndInit();
+			((System.ComponentModel.ISupportInitialize)pcbUsuarios).EndInit();
+			ResumeLayout(false);
+		}
 
-        #endregion
+		#endregion
 
-        private PictureBox pcbMenu;
+		private PictureBox pcbMenu;
         private PictureBox pcbTitulo;
         private PictureBox pcbAlumnos;
         private PictureBox pcbCarreras;
         private PictureBox pcbUsuarios;
         private PictureBox pcbDocentes;
         private Panel pnlContenedor;
-        private PictureBox pcbExit;
+        private PictureBox pcbTutores
+		;
     }
 }
---
+++ prySistemaEscolar/frmCarreras.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace prySistemaEscolar
+{
+	public partial class frmCarreras : Form
+	{
+		clsCarreras carreras;
+
+		public frmCarreras()
+		{
+			InitializeComponent();
+			carreras = new clsCarreras();
+			dgvCarreras.DataSource = null;
+			dgvCarreras.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+			dgvCarreras.DataSource = carreras.CargarDataGrid();
+
+		}
+
+		private void InitializeComponent()
+		{
+			DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+			DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+			pcbLogo = new PictureBox();
+			lblTitulo = new Label();
+			pnlAgrupaControles = new Panel();
+			btnEliminar = new Button();
+			btnGuardar = new Button();
+			btnNuevo = new Button();
+			txtDescripcion = new TextBox();
+			txtNombre = new TextBox();
+			dgvCarreras = new DataGridView();
+			textBox1 = new TextBox();
+			((ISupportInitialize)pcbLogo).BeginInit();
+			pnlAgrupaControles.SuspendLayout();
+			((ISupportInitialize)dgvCarreras).BeginInit();
+			SuspendLayout();
+			// 
+			// pcbLogo
+			// 
+			pcbLogo.BackColor = SystemColors.ControlLightLight;
+			pcbLogo.Image = Properties.Resources._5257028;
+			pcbLogo.Location = new Point(742, 26);
+			pcbLogo.Name = "pcbLogo";
+			pcbLogo.Size = new Size(74, 65);
+			pcbLogo.SizeMode = PictureBoxSizeMode.StretchImage;
+			pcbLogo.TabIndex = 0;
+			pcbLogo.TabStop = false;
+			// 
+			// lblTitulo
+			// 
+			lblTitulo.AutoSize = true;
+			lblTitulo.BackColor = SystemColors.ControlLightLight;
+			lblTitulo.Font = new Font("Segoe UI", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			lblTitulo.Location = new Point(475, 26);
+			lblTitulo.Name = "lblTitulo";
+			lblTitulo.Size = new Size(214, 65);
+			lblTitulo.TabIndex = 1;
+			lblTitulo.Text = "Carreras";
+			// 
+			// pnlAgrupaControles
+			// 
+			pnlAgrupaControles.BackColor = SystemColors.ControlLightLight;
+			pnlAgrupaControles.Controls.Add(btnEliminar);
+			pnlAgrupaControles.Controls.Add(btnGuardar);
+			pnlAgrupaControles.Controls.Add(btnNuevo);
+			pnlAgrupaControles.Controls.Add(txtDescripcion);
+			pnlAgrupaControles.Controls.Add(txtNombre);
+			pnlAgrupaControles.Location = new Point(314, 94);
+			pnlAgrupaControles.Name = "pnlAgrupaControles";
+			pnlAgrupaControles.Size = new Size(624, 201);
+			pnlAgrupaControles.TabIndex = 2;
+			// 
+			// btnEliminar
+			// 
+			btnEliminar.BackgroundImage = Properties.Resources.imagen3;
+			btnEliminar.BackgroundImageLayout = ImageLayout.Stretch;
+			btnEliminar.Location = new Point(376, 140);
+			btnEliminar.Name = "btnEliminar";
+			btnEliminar.Size = new Size(105, 46);
+			btnEliminar.TabIndex = 4;
+			btnEliminar.UseVisualStyleBackColor = true;
+			// 
+			// btnGuardar
+			// 
+			btnGuardar.BackgroundImage = Properties.Resources.imagen2;
+			btnGuardar.BackgroundImageLayout = ImageLayout.Stretch;
+			btnGuardar.Location = new Point(227, 140);
+			btnGuardar.Name = "btnGuardar";
+			btnGuardar.Size = new Size(105, 46);
+			btnGuardar.TabIndex = 3;
+			btnGuardar.UseVisualStyleBackColor = true;
+			// 
+			// btnNuevo
+			// 
+			btnNuevo.BackgroundImage = Properties.Resources.Imagen4;
+			btnNuevo.BackgroundImageLayout = ImageLayout.Stretch;
+			btnNuevo.Location = new Point(77, 140);
+			btnNuevo.Name = "btnNuevo";
+			btnNuevo.Size = new Size(105, 46);
+			btnNuevo.TabIndex = 2;
+			btnNuevo.UseVisualStyleBackColor = true;
+			// 
+			// txtDescripcion
+			// 
+			txtDescripcion.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			txtDescripcion.Location = new Point(111, 84);
+			txtDescripcion.Name = "txtDescripcion";
+			txtDescripcion.PlaceholderText = "Escribir una descrpcion de la carrera";
+			txtDescripcion.Size = new Size(370, 33);
+			txtDescripcion.TabIndex = 1;
+			// 
+			// txtNombre
+			// 
+			txtNombre.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			txtNombre.Location = new Point(111, 41);
+			txtNombre.Name = "txtNombre";
+			txtNombre.PlaceholderText = "Escribir el nombre de la carrera";
+			txtNombre.Size = new Size(370, 33);
+			txtNombre.TabIndex = 0;
+			// 
+			// dgvCarreras
+			// 
+			dgvCarreras.AllowUserToAddRows = false;
+			dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle1.BackColor = Color.FromArgb(255, 255, 192);
+			dataGridViewCellStyle1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			dataGridViewCellStyle1.ForeColor = SystemColors.WindowText;
+			dataGridViewCellStyle1.SelectionBackColor = Color.Teal;
+			dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+			dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+			dgvCarreras.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+			dgvCarreras.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+			dataGridViewCellStyle2.BackColor = Color.Teal;
+			dataGridViewCellStyle2.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+			dataGridViewCellStyle2.ForeColor = Color.White;
+			dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(192, 192, 255);
+			dataGridViewCellStyle2.SelectionForeColor = Color.Black;
+			dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+			dgvCarreras.DefaultCellStyle = dataGridViewCellStyle2;
+			dgvCarreras.EnableHeadersVisualStyles = false;
+			dgvCarreras.Location = new Point(314, 342);
+			dgvCarreras.Name = "dgvCarreras";
+			dgvCarreras.Size = new Size(624, 133);
+			dgvCarreras.TabIndex = 3;
+			// 
+			// textBox1
+			// 
+			textBox1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+			textBox1.Location = new Point(735, 301);
+			textBox1.Name = "textBox1";
+			textBox1.PlaceholderText = "Buscar Carrera";
+			textBox1.Size = new Size(203, 35);
+			textBox1.TabIndex = 4;
+			// 
+			// frmCarreras
+			// 
+			BackgroundImage = Properties.Resources.imagen6;
+			BackgroundImageLayout = ImageLayout.Stretch;
+			ClientSize = new Size(1249, 499);
+			Controls.Add(textBox1);
+			Controls.Add(dgvCarreras);
+			Controls.Add(pnlAgrupaControles);
+			Controls.Add(lblTitulo);
+			Controls.Add(pcbLogo);
+			DoubleBuffered = true;
+			Name = "frmCarreras";
+			Text = "Registro de Carreras";
+			((ISupportInitialize)pcbLogo).EndInit();
+			pnlAgrupaControles.ResumeLayout(false);
+			pnlAgrupaControles.PerformLayout();
+			((ISupportInitialize)dgvCarreras).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+
+		}
+
+		private PictureBox pcbLogo;
+		private Label lblTitulo;
+		private Button btnEliminar;
+		private Button btnGuardar;
+		private Button btnNuevo;
+		private TextBox txtDescripcion;
+		private TextBox txtNombre;
+		private DataGridView dgvCarreras;
+		private TextBox textBox1;
+		private Panel pnlAgrupaControles;
+	}
+}
